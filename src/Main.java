8ab5b6cfb829f460671c5227599fed3377a1da29
liz15243 <lizethespi11@gmail.com>
2022-03-23 21:02:25 -0600

    Agregar en el Main el objeto con la Herencia del Padre en el Hijo

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Mounstruos Syllu = new Mounstruos(
+                "Sylly",
+                1000,
+                4,
+                true,
+                false,
+                "Azul",
+                new ArrayList<>(Arrays.asList("Carismatico","Alto","Fuerte Rugido")));
+    }
+}
